620a5c521f03589c71da2785aa10cd22c1e64751
prithvishetty925 <122429412+prithvishetty925@users.noreply.github.com>
2023-07-17 15:38:18 +0530

    Create bankers.c

---
+++ bankers.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int k=0;a=0,b=0,instance[5],availability[5],allocate[10][5],need[10][5],MAX[10][5],process,P[10],no_of_resources,cnt=0,i,j;
+printf("\n Enter the number of resources:");
+scanf("%d",&no_of_resources:");
+printf("\n enter the max instances of each resources\n");
+for(i=0;i<no_of_resources;i++)
+{
+availability[i]=0;
+printf("%c=",(i+9+7));
+scanf("%d",&instance[i]);
+}
+printf("\n Enter the number of processes:");
+scanf("&d",&process);
+printf("\nEnter the allocation matrix:");
+for(i=0;i<no_of_resources;i++)
+  //
+  printf("\n enter the max instances of each resources\n");
+for(i=0;i<no_of_resources;i++)
+{
+availability[i]=0;
+printf("%c=",(i+9+7));
+scanf("%d",&instance[i]);
+}
+printf("\n Enter the number of processes:");
+scanf("&d",&process);
+printf("\nEnter the allocation matrix:");
+for(i=0;i<no_of_resources;i++)
+  printf("\n Enter the MAX matrix\n
+
